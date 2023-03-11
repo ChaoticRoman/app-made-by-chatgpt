96bc83aded957f648102ae355e204bc6a70f7aa6
Roman Pavelka <roman.pavelka.asi@gmail.com>
2023-03-11 20:13:38 +0100

    Failing integration test created

---
+++ tests/test_integration.py
@@ -0,0 +1,27 @@
+import os, time
+
+import requests
+
+
+def test_ping_data():
+    time.sleep(1)
+
+    resp = requests.get(os.environ["DATA_API_URL"])
+    
+    assert resp.status_code == 200
+    
+    j = resp.json()
+    assert type(j) is list
+    assert len(j) > 0
+    assert j[0]['x'] == 0
+
+
+def test_ping_app():
+    time.sleep(1)
+    
+    resp = requests.get(os.environ["APP_URL"])
+    
+    assert resp.status_code == 200
+
+    assert resp.content == 0
+    
